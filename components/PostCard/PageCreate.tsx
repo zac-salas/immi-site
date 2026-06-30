87a15b277ecaf334ab8c4125a16dc5a045b247f8
ZacSalasDesign <Z.salasnc@gmail.com>
2026-06-30 17:20:08 -0400

    fixing routing

--- components/PostCard/PageCreate.tsx
+++ components/PostCard/PageCreate.tsx
@@ -2,10 +2,9 @@
 
 
 
-
-
+import type { Page } from '@/components/PostCard/Shared'
 import PostcardBuilder from '@/components/PostcardBuilder'
 
-export default function PageCreate() {
-  return <PostcardBuilder />
+export default function PageCreate({ setPage }: { setPage: (p: Page) => void }) {
+  return <PostcardBuilder setPage={setPage} />
 }
